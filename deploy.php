0a25f81b9a43cd4d796e267cf53825baf8dfc3a5
zerocool <htmlshaman@protonmail.com>
2025-01-10 11:30:19 -0500

    added .env.local

--- deploy.php
+++ deploy.php
@@ -1,15 +1,51 @@
-<?php
+<?php 
+
+
+
 namespace Deployer;
 
 require 'recipe/symfony.php';
 
+// Config
+
+set('repository', 'git@github.com:htmlshaman1/sideproject.git');
 host('percent-increase.com')
     ->set('remote_user', 'zerocool')
-    ->set('deploy_path', '/var/www/test');
+    ->set('deploy_path', '/var/www/test')
+    ->set('env', ['APP_ENV' => 'prod']);
+    
+set('composer_options', '--no-dev --optimize-autoloader --no-scripts');
 
-set('repository', 'file://home/zerocool/Desktop/sideproject/');
-add('shared_files', ['.env']);
-add('shared_dirs', ['var/log', 'var/sessions']);
-add('writable_dirs', ['var']);
+set('shared_dirs', [
+    'var/log',
+    'vendor',
+    'uploads',
+    'public/media',
+]);
+set('writable_dirs', [
+    'var',
+    'var/cache',
+    'var/log',
+    'var/sessions',
+    'uploads',
+    'node_ms',
+    'public/media',
+]);
+
+
+
+set('git_tty', false);
+set('use_relative_symlink', false);
+// Hosts
+
+host('redacted')
+    ->set('remote_user', 'app')
+    ->set('deploy_path', 'redacted');
+
+// Hooks
 
 after('deploy:failed', 'deploy:unlock');
+
+
+
+
